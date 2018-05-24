44b777ed5841bab5da9ad9cfad0527dcc11507e8
Dave Jones <dave.jones@hmcts.net>
2018-05-24 11:33:58 +0100

    PRO-2831: Removed redundant call from BusinessApplication.

--- src/main/java/uk/gov/hmcts/probate/BusinessApplication.java
+++ src/main/java/uk/gov/hmcts/probate/BusinessApplication.java
@@ -1,16 +1,18 @@
 package uk.gov.hmcts.probate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.client.RestTemplate;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,9 +25,6 @@ import uk.gov.hmcts.probate.services.idgeneration.IdGeneratorService;
 import uk.gov.hmcts.probate.services.idgeneration.strategy.PinStrategy;
 import uk.gov.hmcts.probate.services.idgeneration.strategy.ProbateStrategy;
 import uk.gov.service.notify.NotificationClient;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @Configuration
