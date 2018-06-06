e716aaf3bdc8e1ed6910481759e5d3490926fd7f
Dave Jones <dave.jones@hmcts.net>
2018-06-06 11:47:05 +0100

    Revert "PRO-3028: Updated to simplify JSON returned for custom health service including removing URL being used."
    This reverts commit c6636d50c9a6cc5cce461eb8bd4d913fc715a388.
    
    Reverting change to remove URL / message from health endpoint.

--- src/test/java/uk/gov/hmcts/probate/services/business/health/BusinessHealthIndicatorTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/business/health/BusinessHealthIndicatorTest.java
@@ -18,6 +18,8 @@ import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.UnknownHttpStatusCodeException;
 
+import uk.gov.hmcts.probate.services.business.health.BusinessHealthIndicator;
+
 @RunWith(MockitoJUnitRunner.class)
 public class BusinessHealthIndicatorTest {
 
@@ -29,20 +31,21 @@ public class BusinessHealthIndicatorTest {
     @Mock
     private ResponseEntity<String> mockResponseEntity;
     
-    private BusinessHealthIndicator businesstHealthIndicator;
+    private BusinessHealthIndicator businessHealthIndicator;
     
     @Before
     public void setUp() {
-    	businesstHealthIndicator = new BusinessHealthIndicator(URL, mockRestTemplate);
+    	businessHealthIndicator = new BusinessHealthIndicator(URL, mockRestTemplate);
     }
 
 	@Test
     public void shouldReturnStatusOfUpWhenHttpStatusIsOK() {
         when(mockRestTemplate.getForEntity(URL + "/health", String.class)).thenReturn(mockResponseEntity);      
         when(mockResponseEntity.getStatusCode()).thenReturn(HttpStatus.OK);
-        Health health = businesstHealthIndicator.health();
+        Health health = businessHealthIndicator.health();
 
         assertThat(health.getStatus(), is(Status.UP));
+        assertThat(health.getDetails().get("url"), is(URL));
     }
 
 	@Test
@@ -50,29 +53,37 @@ public class BusinessHealthIndicatorTest {
         when(mockRestTemplate.getForEntity(URL + "/health", String.class)).thenReturn(mockResponseEntity);
         when(mockResponseEntity.getStatusCode()).thenReturn(HttpStatus.NO_CONTENT);
         when(mockResponseEntity.getStatusCodeValue()).thenReturn(HttpStatus.NO_CONTENT.value());
-        Health health = businesstHealthIndicator.health();
+        Health health = businessHealthIndicator.health();
 
         assertThat(health.getStatus(), is(Status.DOWN));
-        assertThat(health.getDetails().get("error"), is("HTTP Status: 204"));
+        assertThat(health.getDetails().get("url"), is(URL));
+        assertThat(health.getDetails().get("message"), is("HTTP Status code not 200"));
+        assertThat(health.getDetails().get("exception"), is("HTTP Status: 204"));
     }
 
     @Test
     public void shouldReturnStatusOfDownWhenResourceAccessExceptionIsThrown() {
         final String message = "EXCEPTION MESSAGE";
         when(mockRestTemplate.getForEntity(URL + "/health", String.class)).thenThrow(new ResourceAccessException(message));
-        Health health = businesstHealthIndicator.health();
-        
+
+        Health health = businessHealthIndicator.health();
+
         assertThat(health.getStatus(), is(Status.DOWN));
-        assertThat(health.getDetails().get("error"), is("Connection failed with ResourceAccessException"));
+        assertThat(health.getDetails().get("url"), is(URL));
+        assertThat(health.getDetails().get("message"), is(message));
+        assertThat(health.getDetails().get("exception"), is("ResourceAccessException"));
     }
 
     @Test
     public void shouldReturnStatusOfDownWhenHttpStatusCodeExceptionIsThrown() {
         when(mockRestTemplate.getForEntity(URL + "/health", String.class)).thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
-        Health health = businesstHealthIndicator.health();
-        
+
+        Health health = businessHealthIndicator.health();
+
         assertThat(health.getStatus(), is(Status.DOWN));
-        assertThat(health.getDetails().get("error"), is("HTTP Status: 400"));
+        assertThat(health.getDetails().get("url"), is(URL));
+        assertThat(health.getDetails().get("message"), is("400 BAD_REQUEST"));
+        assertThat(health.getDetails().get("exception"), is("HttpStatusCodeException - HTTP Status: 400"));
     }
 
     @Test
@@ -80,9 +91,12 @@ public class BusinessHealthIndicatorTest {
         final String statusText = "status text";
         when(mockRestTemplate.getForEntity(URL + "/health", String.class))
                 .thenThrow(new UnknownHttpStatusCodeException(1000, statusText, null, null, null));
-        Health health = businesstHealthIndicator.health();
-        
+
+        Health health = businessHealthIndicator.health();
+
         assertThat(health.getStatus(), is(Status.DOWN));
-        assertThat(health.getDetails().get("error"), is("Connection failed with UnknownHttpStatusCodeException"));
+        assertThat(health.getDetails().get("url"), is(URL));
+        assertThat(health.getDetails().get("message"), is("Unknown status code [1000] status text"));
+        assertThat(health.getDetails().get("exception"), is("UnknownHttpStatusCodeException - " + statusText));
     }
 }
