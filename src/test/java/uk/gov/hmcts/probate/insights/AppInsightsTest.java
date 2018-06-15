c1a23c107ec2d1ef2255128b452de951f8e6d97d
RBaker <rebecca.baker@hmcts.net>
2018-06-15 16:06:27 +0100

    PRO-3059 Added mutation testing, fixed unit test

---
+++ src/test/java/uk/gov/hmcts/probate/insights/AppInsightsTest.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.probate.insights;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.TelemetryContext;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.util.Assert;
+
+import static org.mockito.Mockito.*;
+
+
+public class AppInsightsTest {
+    private AppInsights classUnderTest;
+
+    @Mock
+    private TelemetryClient telemetryClient;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        TelemetryContext telemetryContext = new TelemetryContext();
+        telemetryContext.setInstrumentationKey("some-key");
+        doReturn(telemetryContext).when(telemetryClient).getContext();
+        classUnderTest = new AppInsights(telemetryClient);
+    }
+
+    @Test
+    public void testTelemetry() {
+        TelemetryContext telemetryContext = new TelemetryContext();
+        telemetryContext.setInstrumentationKey("key");
+
+        TelemetryClient telemetryClient = mock(TelemetryClient.class);
+        when(telemetryClient.getContext()).thenReturn(telemetryContext);
+
+        AppInsights appInsights = new AppInsights(telemetryClient);
+
+        Assert.isInstanceOf(AppInsights.class, appInsights);
+    }
+
+}
--- src/test/java/uk/gov/hmcts/probate/services/business/services/GitCommitInfoEndpointTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/business/services/GitCommitInfoEndpointTest.java
@@ -1,49 +1,41 @@
 package uk.gov.hmcts.probate.services.business.services;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.probate.BusinessApplication;
 
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@Configuration
-@TestPropertySource(properties = {"spring.info.git.location=classpath:uk/gov/hmcts/probate/services/business/git-test.properties"})
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+		"spring.info.git.location=classpath:uk/gov/hmcts/probate/services/business/git-test.properties"})
 public class GitCommitInfoEndpointTest {
 
 	private static final String EXPECTED_COMMIT_ID_INFO_RESPONSE = "0773f129ad51c4a23a49fec96fec0888883443f6";
 	private static final String EXPECTED_COMMIT_TIME_INFO_RESPONSE = "2018-05-23T13:59+1234";
-	
+
+	@Autowired
     private MockMvc mockMvc;
-    
-    @Test
-    public void testGitCommitInfoEndpoint()
-            throws Exception {
-    	
-    	try { 
-	    	MvcResult result = this.mockMvc.perform(get("/info"))
-	                .andExpect(status().isOk()).andReturn();	
-	    	String actualInfoEndpointJsonResponse = result.getResponse().getContentAsString(); 
-	    	
-	    	JSONParser jsonParser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-	    	JSONObject responseObj = (JSONObject) jsonParser.parse(actualInfoEndpointJsonResponse);
-	    	JSONObject gitObject = (JSONObject) responseObj.get("git");
-	    	JSONObject commitObject = (JSONObject) gitObject.get("commit");
-	    	
-	    	assertEquals("Test commit id response is correct.",EXPECTED_COMMIT_ID_INFO_RESPONSE, commitObject.getAsString("id"));
-	    	assertEquals("Test commit time response is correct.",EXPECTED_COMMIT_TIME_INFO_RESPONSE, commitObject.getAsString("time"));
-	    	
-    	} catch ( Exception e ) {
-    		e.printStackTrace();
-    	}
-    }
+
+	@MockBean
+	private BusinessApplication businessApplication;
+
+	@Test
+	public void shouldGetGitCommitInfoEndpoint() throws Exception {
+		mockMvc.perform(get("/info"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.git.commit.id").value(EXPECTED_COMMIT_ID_INFO_RESPONSE))
+				.andExpect(jsonPath("$.git.commit.time").value(EXPECTED_COMMIT_TIME_INFO_RESPONSE));
+	}
 }
