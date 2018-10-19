eeae768cd0a1e3eaa7b23fbb8a87dcd1331cca65
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2018-10-19 15:21:33 +0100

    PRO-3549 additional functional unit test

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/CheckSummaryAnswerPDFTest.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/CheckSummaryAnswerPDFTest.java
@@ -20,10 +20,18 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test class to compare the contents of the inbound JSON file against the content of the generated
+ * PDF document. Since we have limited functionality for testing by using PDFBox we simply test to
+ * see if the strings of content exist in the pdf document when its generated.
+ *
+ * We concaternate the question and answer together before we look for the corresponding pdf string.
+ * When we have a multi answer question we will look for all strings answers belonging to that question.
+ */
 @RunWith(SerenityRunner.class)
 public class CheckSummaryAnswerPDFTest extends IntegrationTestBase {
 
-    private static final String SIMPLE_SUMMARY = "checkAnswersSimpleSummary";
+    private static final String SIMPLE_SUMMARY = "checkAnswersSimpleSummary.json";
     private static final String MULTITIPLE_EXECUTORS = "checkAnswersMultipleExecutorsSummary.json";
     private final String CHECK_ANSWERS_SUMMARY_PDF_URL = "/businessDocument/generateCheckAnswersSummaryPDF";
 
@@ -31,18 +39,16 @@ public class CheckSummaryAnswerPDFTest extends IntegrationTestBase {
     @Test
     public void shouldPassSimpleSummary() throws Exception {
         String pdfContentAsString = pdfContentAsString(SIMPLE_SUMMARY, CHECK_ANSWERS_SUMMARY_PDF_URL);
-        CheckAnswersSummary checkAnswersSummary = getCheckAnswersSummaryFromJSON(MULTITIPLE_EXECUTORS);
+        CheckAnswersSummary checkAnswersSummary = getCheckAnswersSummaryFromJSON(SIMPLE_SUMMARY);
         validatePDFContent(pdfContentAsString, checkAnswersSummary);
     }
 
-//    @Test
-//    public void shouldPassMultipleExecutorsSummary() throws Exception {
-//        String pdfContentAsString = pdfContentAsString(MULTITIPLE_EXECUTORS, CHECK_ANSWERS_SUMMARY_PDF_URL);
-//        CheckAnswersSummary checkAnswersSummary = getCheckAnswersSummaryFromJSON(MULTITIPLE_EXECUTORS);
-//        validatePDFContent(pdfContentAsString, checkAnswersSummary);
-//    }
-
-
+    @Test
+    public void shouldPassMultipleExecutorsSummary() throws Exception {
+        String pdfContentAsString = pdfContentAsString(MULTITIPLE_EXECUTORS, CHECK_ANSWERS_SUMMARY_PDF_URL);
+        CheckAnswersSummary checkAnswersSummary = getCheckAnswersSummaryFromJSON(MULTITIPLE_EXECUTORS);
+        validatePDFContent(pdfContentAsString, checkAnswersSummary);
+    }
 
     private void validatePDFContent(String pdfContentAsString, CheckAnswersSummary checkAnswersSummary) {
         assertThat(pdfContentAsString, containsString(parsedString(checkAnswersSummary.getPageTitle())));
