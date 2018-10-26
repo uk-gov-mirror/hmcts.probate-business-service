91eef491616c37bc8ffc84ed483f6c6f4fd5c643
ruban72 <ruban72@hotmail.com>
2018-10-26 16:58:18 +0100

    PR0-3550: Initial Template for Declaration

--- src/main/java/uk/gov/hmcts/probate/services/businessdocuments/BusinessDocumentController.java
+++ src/main/java/uk/gov/hmcts/probate/services/businessdocuments/BusinessDocumentController.java
@@ -18,21 +18,22 @@ public class BusinessDocumentController {
     private final PDFGenerationService pdfDocumentGenerationService;
 
 
-    @PostMapping(path = "/generateCheckAnswersSummaryPDF", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PostMapping(path = "/generateCheckAnswersSummaryPDF", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = "application/pdf")
     public byte[] generateCheckAnswersSummaryPDF(@Valid @RequestBody CheckAnswersSummary checkAnswersSummary, @RequestHeader("ServiceAuthorization") String authorization) {
 
         log.info("call to generateCheckAnswersSummaryPDF()");
 
-        final byte[] bytes = pdfDocumentGenerationService.generatePdf(authorization, checkAnswersSummary, DocumentType.CHECK_ANSWERS_SUMMARY);
+        final byte[] bytes = pdfDocumentGenerationService.generatePdf(authorization, checkAnswersSummary,DocumentType.CHECK_ANSWERS_SUMMARY);
         return bytes;
     }
 
-    @PostMapping(path = "/generateLegalDeclarationPDF", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PostMapping(path = "/generateLegalDeclarationPDF", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = "application/pdf")
     public byte[] generateLegalDeclarationPDF(@Valid @RequestBody LegalDeclaration legalDeclaration, @RequestHeader("ServiceAuthorization") String authorization) {
 
-        log.info("call to generateLegalDeclarationPDF()");
+        log.info("call to generateCheckAnswersSummaryPDF()");
 
-        return pdfDocumentGenerationService.generatePdf(authorization, legalDeclaration, DocumentType.LEGAL_DECLARATION);
+        final byte[] bytes = pdfDocumentGenerationService.generatePdf(authorization, legalDeclaration,DocumentType.LEGAL_DECLARATION);
+        return bytes;
     }
 
 }
