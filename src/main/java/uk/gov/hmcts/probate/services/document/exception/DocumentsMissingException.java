e195c2293cb0b5fee20bbc812ab283440643c2a0
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-10-26 15:31:42 +0100

    PRO-3820: changed format for errors being returned

--- src/main/java/uk/gov/hmcts/probate/services/document/exception/DocumentsMissingException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.probate.services.document.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class DocumentsMissingException extends RuntimeException {
-}
--- src/test/java/uk/gov/hmcts/probate/services/document/unit/DocumentServiceTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/document/unit/DocumentServiceTest.java
@@ -4,12 +4,17 @@ import static org.mockito.Mockito.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.internal.ir.ObjectNode;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.probate.services.document.DocumentService;
@@ -17,6 +22,7 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.document.DocumentUploadClientApi;
 import uk.gov.hmcts.reform.document.domain.UploadResponse;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,8 +31,6 @@ public class DocumentServiceTest {
 
     @Mock
     private AuthTokenGenerator authTokenGenerator;
-    @Mock
-    private DocumentUploadClientApi documentUploadClientApi;
     @Mock
     private RestTemplate restTemplate;
 
@@ -38,18 +42,22 @@ public class DocumentServiceTest {
 
     @Before
     public void setUp() {
-        documentService = new DocumentService(authTokenGenerator, documentUploadClientApi, restTemplate);
+        documentService = new DocumentService(authTokenGenerator, restTemplate);
     }
 
     @Test
-    public void shouldUploadDocumentsToEvidenceManagement() {
+    public void shouldUploadDocumentsToEvidenceManagement() throws IOException {
         MultipartFile file = mock(MultipartFile.class);
+        byte[] bytes = new byte[10];
+        when(file.getBytes()).thenReturn(bytes);
+        when(file.getContentType()).thenReturn("application/pdf");
         List<MultipartFile> files = Collections.singletonList(file);
         when(authTokenGenerator.generate()).thenReturn(AUTH_TOKEN);
         UploadResponse uploadResponse = mock(UploadResponse.class);
-        when(documentService.upload(files, DUMMY_OAUTH_2_TOKEN, USER_ID)).thenReturn(uploadResponse);
+        HttpEntity httpEntity = mock(HttpEntity.class);
+        when(documentService.upload(DUMMY_OAUTH_2_TOKEN, AUTH_TOKEN, USER_ID, files)).thenReturn(uploadResponse);
 
-        UploadResponse actualUploadResponseEmbedded = documentService.upload(files, DUMMY_OAUTH_2_TOKEN, USER_ID);
+        UploadResponse actualUploadResponseEmbedded = documentService.upload(DUMMY_OAUTH_2_TOKEN, AUTH_TOKEN, USER_ID, files);
         assertThat(actualUploadResponseEmbedded, equalTo(uploadResponse));
     }
 
@@ -57,9 +65,9 @@ public class DocumentServiceTest {
     public void shouldDeleteDocumentFromEvidenceManagement() {
         ResponseEntity response = mock(ResponseEntity.class);
         when(authTokenGenerator.generate()).thenReturn(AUTH_TOKEN);
-        when(documentService.delete("file")).thenReturn(response);
+        when(documentService.delete(USER_ID, "file")).thenReturn(response);
 
-        ResponseEntity actualResponse = documentService.delete("file");
+        ResponseEntity actualResponse = documentService.delete(USER_ID, "file");
         assertThat(actualResponse, equalTo(response));
     }
 }
