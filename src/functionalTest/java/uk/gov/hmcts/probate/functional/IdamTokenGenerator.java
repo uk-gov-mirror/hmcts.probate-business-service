375fb455091f3d1da634f53091ce239a1a02168f
Dhruv Patel <dhruv.patel20@gmail.com>
2018-12-07 13:56:49 +0000

    PRO-3820: New user for functional tests.

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/IdamTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/IdamTokenGenerator.java
@@ -62,7 +62,7 @@ public class IdamTokenGenerator {
 
     private String generateClientCode() {
         String code = "";
-        final String encoded = Base64.getEncoder().encodeToString((idamUsername + ":" + idamPassword).getBytes());
+        final String encoded = Base64.getEncoder().encodeToString(("testABC@TEST.COM:Probate123").getBytes());
         code = RestAssured.given().baseUri(idamUserBaseUrl)
                 .header("Authorization", "Basic " + encoded)
                 .post("/oauth2/authorize?response_type=code&client_id=probate&redirect_uri=" + redirectUri)
