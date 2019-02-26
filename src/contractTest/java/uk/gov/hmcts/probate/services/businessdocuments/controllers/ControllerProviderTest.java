dae60247a7d32e2216169e68302983bd3d961471
ruban72 <ruban72@hotmail.com>
2019-02-26 16:13:52 +0000

    Added pact provider tests for Orchestrator

---
+++ src/contractTest/java/uk/gov/hmcts/probate/services/businessdocuments/controllers/ControllerProviderTest.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.probate.services.businessdocuments.controllers;
+
+import au.com.dius.pact.provider.junit.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@PactBroker(scheme = "${pact.broker.scheme}",host = "${pact.broker.baseUrl}", port = "${pact.broker.port}", tags={"${pact.broker.consumer.tag}", "latest", "master"})
+@IgnoreNoPactsToVerify
+abstract public class ControllerProviderTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Before
+    public void setUpTest() {
+        System.getProperties().setProperty("pact.verifier.publishResults", "true");
+    }
+
+    protected JSONObject createJsonObject(String fileName) throws JSONException, IOException {
+        File file = getFile(fileName);
+        String jsonString = new String(Files.readAllBytes(file.toPath()));
+        return new JSONObject(jsonString);
+    }
+
+
+    private File getFile(String fileName) throws FileNotFoundException {
+        return ResourceUtils.getFile(this.getClass().getResource("/json/" + fileName));
+    }
+}
