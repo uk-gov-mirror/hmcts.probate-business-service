1406c9f45f2e12abd07228d9164586f21f9696d3
ruban72 <ruban72@hotmail.com>
2020-01-03 17:41:01 +0000

    WLA-88: Remove logging (Sonar issue)

--- src/main/java/uk/gov/hmcts/probate/services/invitation/controllers/InvitationController.java
+++ src/main/java/uk/gov/hmcts/probate/services/invitation/controllers/InvitationController.java
@@ -1,23 +1,26 @@
 package uk.gov.hmcts.probate.services.invitation.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.UnsupportedEncodingException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.probate.services.idgeneration.IdGeneratorService;
 import uk.gov.hmcts.probate.services.invitation.InvitationService;
 import uk.gov.hmcts.reform.probate.model.multiapplicant.Invitation;
 import uk.gov.service.notify.NotificationClientException;
 
-import javax.validation.Valid;
-import javax.ws.rs.core.MediaType;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
+import javax.validation.Valid;
+import javax.ws.rs.core.MediaType;
 
 @RestController
 @Tag(name = "Invite Generation Service")
@@ -35,8 +38,8 @@ public class InvitationController {
 
     @RequestMapping(path = "/invite/bilingual", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON)
     public String inviteBilingual(@Valid @RequestBody Invitation encodedInvitation,
-                         BindingResult bindingResult,
-                         @RequestHeader("Session-Id") String sessionId) throws NotificationClientException, UnsupportedEncodingException {
+                                  BindingResult bindingResult,
+                                  @RequestHeader("Session-Id") String sessionId) throws NotificationClientException, UnsupportedEncodingException {
         return sendInvitation(encodedInvitation, bindingResult, sessionId, Boolean.TRUE);
     }
 
@@ -59,9 +62,9 @@ public class InvitationController {
 
     @RequestMapping(path = "/invite/bilingual/{inviteId}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON)
     public String inviteBilingual(@PathVariable("inviteId") String inviteId,
-                         @Valid @RequestBody Invitation invitation,
-                         BindingResult bindingResult,
-                         @RequestHeader("Session-Id") String sessionId) throws NotificationClientException {
+                                  @Valid @RequestBody Invitation invitation,
+                                  BindingResult bindingResult,
+                                  @RequestHeader("Session-Id") String sessionId) throws NotificationClientException {
         invitationService.sendEmail(inviteId, invitation, Boolean.TRUE);
         return inviteId;
     }
