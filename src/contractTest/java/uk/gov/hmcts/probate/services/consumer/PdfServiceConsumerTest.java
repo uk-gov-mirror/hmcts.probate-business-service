efa63a82a2da74108c143f0361f9f36cdcd888ab
ruban72 <ruban72@hotmail.com>
2020-11-11 09:57:42 +0000

    TA-166: Fix spring initialization issues

--- src/contractTest/java/uk/gov/hmcts/probate/services/consumer/PdfServiceConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/probate/services/consumer/PdfServiceConsumerTest.java
@@ -1,26 +1,4 @@
-package uk.gov.hmcts.probate.services.businessdocuments.controllers;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.Mockito.when;
-
-import uk.gov.hmcts.probate.config.PDFServiceConfiguration;
-import uk.gov.hmcts.probate.services.businessdocuments.model.DocumentType;
-import uk.gov.hmcts.probate.services.businessdocuments.services.FileSystemResourceService;
-import uk.gov.hmcts.probate.services.businessdocuments.services.PDFGenerationService;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.probate.model.documents.BusinessDocument;
-import uk.gov.hmcts.reform.probate.model.documents.CheckAnswersSummary;
-import uk.gov.hmcts.reform.probate.model.documents.Section;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+package uk.gov.hmcts.probate.services.consumer;
 
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
@@ -34,30 +12,38 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.fluent.Executor;
 import org.json.JSONException;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.util.ResourceUtils;
+import uk.gov.hmcts.probate.config.PDFServiceConfiguration;
+import uk.gov.hmcts.probate.services.businessdocuments.model.DocumentType;
+import uk.gov.hmcts.probate.services.businessdocuments.services.FileSystemResourceService;
+import uk.gov.hmcts.probate.services.businessdocuments.services.PDFGenerationService;
+import uk.gov.hmcts.reform.pdf.service.client.GeneratePdfRequest;
+import uk.gov.hmcts.reform.probate.model.documents.CheckAnswersSummary;
+import uk.gov.hmcts.reform.probate.model.documents.Section;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @ExtendWith(PactConsumerTestExt.class)
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@PactTestFor(providerName = "rpePdfService_PDFGenerationEndpointV2", port = "8891")
+@PactTestFor(providerName = "rpePdfService_PDFGenerationEndpointV2", port = "5500")
 @PactFolder("pacts")
 @SpringBootTest({
-    "services.pdf.service.url : http://localhost:8891/pdfs"
+    "logging.level.au.com.dius.pact : DEBUG"
 })
 public class PdfServiceConsumerTest {
 
     private static final String SERVICE_AUTHORIZATION_HEADER = "ServiceAuthorization";
+    private static final String HTML = ".html";
 
     @Autowired
     PDFGenerationService pdfGenerationService;
@@ -65,16 +51,10 @@ public class PdfServiceConsumerTest {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    private AuthTokenGenerator serviceTokenGenerator;
-
-    @Mock
-    private CheckAnswersSummary mockCheckAnswersSummary;
-
-    @MockBean
+    @Autowired
     private PDFServiceConfiguration pdfServiceConfiguration;
 
-    @MockBean
+    @Autowired
     private FileSystemResourceService fileSystemResourceService;
 
     private final String someServiceAuthToken = "someServiceAuthToken";
@@ -84,20 +64,15 @@ public class PdfServiceConsumerTest {
     @BeforeEach
     public void setUpEachTest() throws InterruptedException, IOException {
         Thread.sleep(2000);
-
-        try {
-            when(objectMapper.writeValueAsString(Mockito.any(CheckAnswersSummary.class))).thenReturn(someJSON);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
     }
 
     @After
     void teardown() {
         Executor.closeIdleConnections();
     }
-                                                              // TBD consumer 'Name'
-    @Pact(provider = "rpePdfService_PDFGenerationEndpointV2", consumer = "probate_documentGeneratorClient")
+
+    // TBD consumer 'Name'
+    @Pact(provider = "rpePdfService_PDFGenerationEndpointV2", consumer = "probate_businessService")
     RequestResponsePact generatePdfFromTemplate(PactDslWithProvider builder) throws JSONException, IOException {
         // @formatter:off
 
@@ -105,8 +80,8 @@ public class PdfServiceConsumerTest {
             .given("A request to generate a PDF document ")
             .uponReceiving("A request to generate a PDF document ")
             .method("POST")
-            .headers(SERVICE_AUTHORIZATION_HEADER, someServiceAuthToken)
-            .body(createJsonObject(answersSummary()),
+            //.headers(SERVICE_AUTHORIZATION_HEADER, someServiceAuthToken)
+            .body(createJsonObject(buildGenerateDocumentRequest(DocumentType.CHECK_ANSWERS_SUMMARY.getTemplateName(), answersSummary())),
                 "application/vnd.uk.gov.hmcts.pdf-service.v2+json;charset=UTF-8")
             .path("/pdfs")
             .willRespondWith()
@@ -119,16 +94,7 @@ public class PdfServiceConsumerTest {
     @Test
     @PactTestFor(pactMethod = "generatePdfFromTemplate")
     public void verifyGeneratePdfFromTemplatePact() throws IOException, JSONException {
-
-        when(pdfServiceConfiguration.getTemplatesDirectory()).thenReturn("templateDirectory");
-        when(fileSystemResourceService.getFileFromResourceAsString(Mockito.anyString())).thenReturn("templateAsString");
-
-        when(serviceTokenGenerator.generate()).thenReturn(someServiceAuthToken);
-
         byte[] response = pdfGenerationService.generatePdf(answersSummary(), DocumentType.CHECK_ANSWERS_SUMMARY);
-
-        assertThat(response , notNullValue());
-        // TODO other asserts required here.
 
     }
 
@@ -136,7 +102,7 @@ public class PdfServiceConsumerTest {
         return objectMapper.writeValueAsString(obj);
     }
 
-    private CheckAnswersSummary answersSummary(){
+    private CheckAnswersSummary answersSummary() {
         CheckAnswersSummary summary = new CheckAnswersSummary();
         summary.setMainParagraph("paragraph");
         summary.setPageTitle("title");
@@ -149,5 +115,15 @@ public class PdfServiceConsumerTest {
         summary.setSections(sectionList);
 
         return summary;
+    }
+
+    private GeneratePdfRequest buildGenerateDocumentRequest(String templateName, CheckAnswersSummary businessDocument)
+        throws JsonProcessingException {
+        String templatePath = pdfServiceConfiguration.getTemplatesDirectory() + templateName + HTML;
+        String templateAsString = fileSystemResourceService.getFileFromResourceAsString(templatePath);
+
+        Map<String, Object> paramMap = pdfGenerationService.asMap(objectMapper.writeValueAsString(businessDocument));
+        return new GeneratePdfRequest(templateAsString, paramMap);
+
     }
 }
