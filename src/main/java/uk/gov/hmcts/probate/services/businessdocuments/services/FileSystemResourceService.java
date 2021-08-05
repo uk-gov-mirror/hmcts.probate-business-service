72388bb836736b90caebb6d280c683514c7dde85
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-08-05 16:55:53 +0100

    Dtspb 2066 fortify fixes (#601)
    * DTSPB-2066 Resolve the Fortify vulnerabilities
    
    * DTSPB-2066 Implement using try with parameter
    
    * Implementation with nio to fix sonar issues
    
    * Fix the sonar failure , remove file and dir prefix
    
    * Suppress sonar warning as its irrelevant
    
    * Updated the java chart version
    
     Please enter the commit message for your changes. Lines starting
     ignored, and an empty message aborts the commit.
    
    * Bumping chart version
    
    * Revert the java chart version
    
    Co-authored-by: hmcts-jenkins-probate <62423138+hmcts-jenkins-probate[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/probate/services/businessdocuments/services/FileSystemResourceService.java
+++ src/main/java/uk/gov/hmcts/probate/services/businessdocuments/services/FileSystemResourceService.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.probate.services.businessdocuments.services;
 
-import com.google.common.io.Files;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
@@ -8,37 +8,46 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.probate.services.businessdocuments.exceptions.FileSystemException;
 
-import java.io.File;
+
+
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Slf4j
 @Component
+@SuppressWarnings("squid:S5443")
 public class FileSystemResourceService {
 
     public static final String BUSINESS_DOCUMENT_TEMPLATE_COULD_NOT_BE_FOUND =
         "Business Document template could not be found";
 
+
     public Optional<FileSystemResource> getFileSystemResource(String resourcePath) {
-        final String secureTempDir = Files.createTempDir().getAbsolutePath();
-        final File secureDir = new File(secureTempDir);
-        return Optional.ofNullable(this.getClass().getClassLoader().getResourceAsStream(resourcePath))
+        final InputStream ins = this.getClass().getClassLoader().getResourceAsStream(resourcePath);
+        return Optional.ofNullable(ins)
             .map(in -> {
-                try {
-                    File tempFile =
-                        File.createTempFile(String.valueOf(in.hashCode()), ".html", secureDir);
-                    secureDir.deleteOnExit();
-                    tempFile.deleteOnExit();
-                    FileOutputStream out = new FileOutputStream(tempFile);
+                FileOutputStream out = null;
+                try (ins) {
+                    Path secureDir = Files.createTempDirectory("");
+                    Path tempFile = Files.createTempFile(
+                        Paths.get(secureDir.toAbsolutePath().toString()),"",".html");
+                    secureDir.toFile().deleteOnExit();
+                    tempFile.toFile().deleteOnExit();
+                    out = new FileOutputStream(tempFile.toFile());
                     IOUtils.copy(in, out);
-                    return new FileSystemResource(tempFile);
+                    return new FileSystemResource(tempFile.toFile());
                 } catch (IOException e) {
                     log.error("File system [ {} ] could not be found", resourcePath, e);
                     throw new FileSystemException(BUSINESS_DOCUMENT_TEMPLATE_COULD_NOT_BE_FOUND, e);
                 }
             });
+
     }
 
     public String getFileFromResourceAsString(String resourcePath) {
