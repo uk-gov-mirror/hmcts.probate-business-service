34d39b03894182f3d6b118f8e99def3eee7569d5
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-02-03 20:37:48 +0000

    DTSPB-4511 Fix html

--- src/main/java/uk/gov/hmcts/probate/services/businessdocuments/services/PDFGenerationService.java
+++ src/main/java/uk/gov/hmcts/probate/services/businessdocuments/services/PDFGenerationService.java
@@ -9,7 +9,6 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.tagging.StandardRoles;
 import com.itextpdf.kernel.pdf.tagutils.TagTreePointer;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.IElement;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
@@ -22,7 +21,10 @@ import uk.gov.hmcts.probate.services.businessdocuments.model.DocumentType;
 import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.hmcts.reform.probate.model.documents.BusinessDocument;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
